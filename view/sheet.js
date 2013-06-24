025ec68cba0b63aeb04cb77fa7347895be2d705e
Pieter van der Eems <eemcorpg@eemco.nl>
2013-06-24 20:58:30 +0200

    GPL licence statement added to source files

--- view/sheet.js
+++ view/sheet.js
@@ -79,3 +79,18 @@ exports.filters = {
         return false;
     }
 };
+//Copyright 2013 Pieter van der Eems
+//    This file is part of EemcoRPG
+//
+//    EemcoRPG is free software: you can redistribute it and/or modify
+//    it under the terms of the Affero GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    EemcoRPG is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    Affero GNU General Public License for more details.
+//
+//    You should have received a copy of the Affero GNU General Public License
+//    along with EemcoRPG.  If not, see <http://www.gnu.org/licenses/>.
